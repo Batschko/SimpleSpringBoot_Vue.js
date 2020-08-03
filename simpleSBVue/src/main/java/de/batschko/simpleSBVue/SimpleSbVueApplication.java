caa8fee230ba95821b32a83c94da1f3f9d92dc5d
Batschko <lukas.h2@hotmail.de>
2020-08-03 21:16:14 +0200

    initial SptringBoot HelloWorld

---
+++ simpleSBVue/src/main/java/de/batschko/simpleSBVue/SimpleSbVueApplication.java
@@ -0,0 +1,13 @@
+package de.batschko.simpleSBVue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSbVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSbVueApplication.class, args);
+	}
+
+}
---
+++ simpleSBVue/src/test/java/de/batschko/simpleSBVue/SimpleSbVueApplicationTests.java
@@ -0,0 +1,13 @@
+package de.batschko.simpleSBVue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSbVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
